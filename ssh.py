1fd6169985d86bcc5d3a2949a64b4551823757a4
Bad Wolf <noel.garcia@openmailbox.org>
2017-10-19 19:11:38 +0200

    Adding logging for ssh.py

--- ssh.py
+++ ssh.py
@@ -6,15 +6,24 @@
   port scans...
 """
 
-import test as geoip    # Import personal geoip
-import parser as func
-import datetime as dt
-import numpy as np
-import sys
 import os
 here = os.path.dirname(os.path.realpath(__file__))  # script folder
 HOME = os.getenv('HOME')
 USER = os.getenv('USER')
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)s:%(levelname)s - %(message)s',
+                    datefmt='%Y/%m/%d-%H:%M:%S',
+                    filename='SSHlog.log', filemode='w')
+LG = logging.getLogger('main')
+
+# dependence on my geoip library, which can be found here:
+# https://github.com/B4dWo1f/bin/blob/master/geoip.py
+import geoip
+import parser as func
+import datetime as dt
+import numpy as np
+import sys
 
 
 ## Standard input to select file and hostname, mainly for debugging
@@ -53,8 +62,10 @@ for l in sshd_logins:
 
 
 ## Extract info from each attempt
+logins = failed_logins + alert_logins + port_scans
+LG.info('Evaluating %s attempts'%(len(logins)))
 attempts = []
-for l in failed_logins + alert_logins + port_scans: # TODO Split contributions
+for l in logins: # TODO Split contributions
    resp = func.parse_attempt(l) #,hostname=hostname)
    if resp != None: attempts.append(resp)
 
@@ -72,17 +83,28 @@ ports = np.asarray(ports)
 
 now = dt.datetime.now()
 LAT,LON,NUM,WHEN = [],[],[],[]
+cont = 0
 for ip in dif_IPs:
    try:   # Try to find local directory of IP-GPS
       resp = os.popen('grep "%s   " %s'%(ip,ips_file)).read()
+      #aux = ' '.join(resp.split()[-3:])
+      #DT = dt.datetime.strptime(aux, '%Y %m %d')
+      #if (now-DT).total_seconds() > 60*60*24*30:
+      #   print('data older than a month')
+      #else: print('data new enough')
+      #exit()
       lat,lon = map(float,resp.split()[1:3])
+      LG.debug('%s from file'%(ip))
    except ValueError:
+      LG.debug('%s from web'%(ip))
       info = geoip.analyze_IP(ip)
       lat,lon = info.coor
       f = open(ips_file,'a')  # This file should be deleted ~ once a month
       f.write(ip+'   '+str(lat)+'   '+str(lon)+'   ')
       f.write(now.strftime('%Y   %m   %d') +'\n')
       f.close()
+   #print('out')
+   #exit()
    num = np.count_nonzero(IPs == ip)
    latest_attempt = np.max(dates[IPs==ip])
    d = (1+(now-latest_attempt).total_seconds())/7200
@@ -90,6 +112,8 @@ for ip in dif_IPs:
    LON.append(lon)
    NUM.append(num)
    WHEN.append( min(1/d,1) )   # (1,0,0,min(1/d,1)))
+   cont += 1
 
 M = np.vstack((LAT,LON,NUM,WHEN)).transpose()
 np.save(here+'/attacks.npy',M)
+LG.info('All done')
